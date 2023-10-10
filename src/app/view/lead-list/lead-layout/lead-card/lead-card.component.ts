ead12e02221aabb28dace499bcf1160abf317e6d
MgAsma <asma@ekfrazo.in>
2023-10-10 11:24:05 +0530

    LEAD LAYOUT MODIFIED

--- src/app/view/lead-list/lead-layout/lead-card/lead-card.component.ts
+++ src/app/view/lead-list/lead-layout/lead-card/lead-card.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatBottomSheet, MatBottomSheetConfig } from '@angular/material/bottom-sheet';
+import { AddLeadComponent } from '../add-lead/add-lead.component';
+import { LeadUploadComponent } from '../lead-upload/lead-upload.component';
 
 @Component({
   selector: 'app-lead-card',
@@ -44,10 +47,49 @@ export class LeadCardComponent implements OnInit {
       emailCount: 0,
       personCounts: [5, 2, 1],
     },
+    {
+      name: 'Student 2',
+      badgeCount: 5,
+      id: '2067638819',
+      status: 'Untouched',
+      leadStatus:'Fresh Lead',
+      erp: true,
+      phoneNumber: 1,
+      chatCount: 1,
+      emailCount: 0,
+      personCounts: [5, 0, 0],
+    },
+    {
+      name: 'Student 3',
+      badgeCount: 2,
+      id: '2067638819',
+      status: 'Untouched',
+      leadStatus:'Fresh Lead',
+      erp: true,
+      phoneNumber: 1,
+      chatCount: 1,
+      emailCount: 0,
+      personCounts: [5, 2, 1],
+    },
     // Add more dummy data as needed
   ];
 
-  constructor(){}
+ 
+  constructor(private _bottomSheet:  MatBottomSheet) {}
+  
+   
+  openBottomSheet(): void {
+    const config: MatBottomSheetConfig = {
+      panelClass: 'lead-bottom-sheet'
+    };
+    this._bottomSheet.open(AddLeadComponent,config);
+  }
+  uploadLeads(): void{
+    const config: MatBottomSheetConfig = {
+      panelClass: 'lead-bottom-sheet'
+    };
+    this._bottomSheet.open(LeadUploadComponent,config);
+  }
   ngOnInit(): void {
   }
 
--- src/app/view/lead-list/lead-layout/lead-layout.component.ts
+++ src/app/view/lead-list/lead-layout/lead-layout.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AddLeadComponent } from './add-lead/add-lead.component';
-import { MatBottomSheet,MatBottomSheetConfig  } from '@angular/material/bottom-sheet';
-import { LeadUploadComponent } from './lead-upload/lead-upload.component';
+
 
 @Component({
   selector: 'app-lead-layout',
@@ -11,21 +9,6 @@ import { LeadUploadComponent } from './lead-upload/lead-upload.component';
 export class LeadLayoutComponent implements OnInit {
 
  
-    constructor(private _bottomSheet:  MatBottomSheet) {}
-  
-   
-    openBottomSheet(): void {
-      const config: MatBottomSheetConfig = {
-        panelClass: 'lead-bottom-sheet'
-      };
-      this._bottomSheet.open(AddLeadComponent,config);
-    }
-    uploadLeads(): void{
-      const config: MatBottomSheetConfig = {
-        panelClass: 'lead-bottom-sheet'
-      };
-      this._bottomSheet.open(LeadUploadComponent,config);
-    }
   ngOnInit(): void {
   }
 
