d372cba2e70041e7d6fcb9c66d6b8b8131c7257e
MgAsma <asma@ekfrazo.in>
2024-04-02 13:37:55 +0530

    leadCard alignment done

--- src/app/view/request-report/report-form/report-form.component.ts
+++ src/app/view/request-report/report-form/report-form.component.ts
@@ -61,9 +61,8 @@ export class ReportFormComponent implements OnInit {
     }))
    }
    getCounselor(){
-    this._baseService.getData(`${environment._user}`).subscribe((res:any)=>{
+    this._baseService.getData(`${environment._user}?lead_exists=True&role_name=counsellor`).subscribe((res:any)=>{
       if(res.results){
-      // this.referredTo = res.results.filter((f:any)=>f.id != this.user_id)
       this.referredTo = res.results
       }
     },((error:any)=>{
