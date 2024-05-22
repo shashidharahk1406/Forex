468973b0fdc52a77e3b581f4c5232ea9536a7870
MgAsma <asma@ekfrazo.in>
2024-05-22 11:53:15 +0530

    counselledby modified

--- src/app/view/lead-list/lead-layout/lead-filter/lead-filter.component.ts
+++ src/app/view/lead-list/lead-layout/lead-filter/lead-filter.component.ts
@@ -144,7 +144,7 @@ queryItems: any;
     )
   }
   getCounselledBy(){
-    let query = "?role_name=${superadmin}"
+    let query = `?role_name=superadmin`
   
     this._baseService.getData(`${environment._user}${query}`).subscribe((res:any)=>{
       if(res){
