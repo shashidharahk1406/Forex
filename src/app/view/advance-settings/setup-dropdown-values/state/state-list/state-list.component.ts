2b24bbf29ed55178e9442c5254ca0dc408f1a6f0
shashidharahk1406 <shashidhara@ekfrazo.in>
2024-05-29 10:07:03 +0530

    followup module issues have been fixed

--- src/app/view/advance-settings/setup-dropdown-values/state/state-list/state-list.component.ts
+++ src/app/view/advance-settings/setup-dropdown-values/state/state-list/state-list.component.ts
@@ -62,7 +62,7 @@ export class StateListComponent implements  AfterViewInit {
   }
   search(){
     if(this.searchValue?.length>0){
-      this.api.getStateSearch(this.searchValue,this.pageSize,this.currentPage).subscribe((resp:any)=>{
+      this.api.getStateSearch(this.searchValue,this.pageSize,this.currentPage=1).subscribe((resp:any)=>{
         this.allState= resp.results;
         this.dataSource = new MatTableDataSource<any>(this.allState);
         this.totalPageLength=resp.total_no_of_record
